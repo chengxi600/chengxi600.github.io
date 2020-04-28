152e0339d194ea08e91833c3d1798ed297abb6a3
Cheng Xi Tsou <chengxi600@gmail.com>
2020-04-29 01:45:25 +0800

    home final

---
+++ js/home.js
@@ -0,0 +1,14 @@
+//if yOffset is bigger than viewport height, fix navbar to top
+//else, no fix
+
+var $navbar = document.querySelector('.navbar');
+const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+window.addEventListener('scroll', function(e) {
+    if(window.pageYOffset > vh) {
+        $navbar.classList.add('fixed-top');
+    } else {
+        $navbar.classList.remove('fixed-top');
+    }
+});
